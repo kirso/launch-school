42bc9adb7b120cae4a948a3fb3750c3cb907abbe
Kirill So <kirill.sofronov@gmail.com>
2023-05-11 22:28:45 +0800

    Add Arrays exercise files and update README

---
+++ intro-to-javascript/8-arrays/arrays.js
@@ -0,0 +1,42 @@
+// let array = [1, 2, 3];
+
+// array.push("a");
+
+// console.log(array);
+
+// array.concat(42, "abc");
+// console.log(array);
+
+// let newArray = array.concat(42, "abc");
+// console.log(newArray);
+
+// newArray.forEach((e) => {
+// 	console.log(e + 2);
+// });
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2];
+
+// numbers.filter((num) => num > 4);
+
+// numbers;
+
+// // Compare the arrays
+
+// function arraysEqual(arr1, arr2) {
+// 	if (arr1.length !== arr2.length) return false;
+
+// 	for (let i = 0; i < arr1.length; i += 1) {
+// 		if (arr1[i] !== arr2[i]) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// console.log(arraysEqual([1, 2, [2, 3]], [1, 2, [2, 3]]));
+// console.log(arraysEqual([1, 2, 2], [1, 2, 3]));
+
+let a = ["e", "c", "h", "b", "d", "a"];
+a.sort();
+
+console.log(a.sort());
